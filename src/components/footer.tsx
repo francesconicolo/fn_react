9da0761bd937c1a7be249ded3bd7e5e8bbe6799a
Francesco Nicolo <nicolo.francesco.fn@gmail.com>
2023-09-09 14:43:12 +0200

    Aggiunto footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
-
+import React from "react";
+import "../assets/css/page.css";
 export default function Footer() {
   return (
-    <div className='bg-black h-[300px] w-full'>footer</div>
-  )
+    <div className="bg-black h-[500px] w-full text-white font-Updock flex justify-center items-center text-[6rem]">
+      The End
+      <span className="animazioneCustom text-[10rem] pl-5">?</span>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors:{
-        Lantiquewhite: '#FEEEDFff',
-        Dantiflashwhite: '#EEEEEEff',
-        
-        Lhunyadiyellow: '#FAC166ff',
-        Dyaleblue: '#1D387Bff',
-        
-        Lfalured: '#741C1Cff',
-        Dpennblue: '#141552ff',
+      colors: {
+        Lantiquewhite: "#FEEEDFff",
+        Dantiflashwhite: "#EEEEEEff",
 
+        Lhunyadiyellow: "#FAC166ff",
+        Dyaleblue: "#1D387Bff",
+
+        Lfalured: "#741C1Cff",
+        Dpennblue: "#141552ff",
       },
-      fontFamily:{
-        press:['var(--font-pressStart2p)'],
-        rye:['var(--font-rye)'],
-        homemade:['var(--font-homemadeApple)'],
-        peralta:['var(--font-peralta)'],
-        carattere:['var(--font-carattere)']
-      }
+      fontFamily: {
+        press: ["var(--font-pressStart2p)"],
+        rye: ["var(--font-rye)"],
+        homemade: ["var(--font-homemadeApple)"],
+        peralta: ["var(--font-peralta)"],
+        carattere: ["var(--font-carattere)"],
+        Updock: ["var(--font-updock)"],
+      },
     },
   },
   plugins: [],
-}
+};
